dc370a956e1d687a645efa92cb4d67980ff513d6
michaelgoodmels <michael.good@outlook.com>
2023-09-04 20:14:34 +0200

    first stage
    ...

---
+++ ggT.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+// Funktion, um den GGT mithilfe des euklidischen Algorithmus zu finden
+int findeGGT(int a, int b) {
+    while (b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+
+int main() {
+    int zahl1, zahl2;
+
+    // Eingabe der beiden Zahlen
+    printf("Geben Sie die erste Zahl ein: ");
+    scanf("%d", &zahl1);
+
+    printf("Geben Sie die zweite Zahl ein: ");
+    scanf("%d", &zahl2);
+
+    // Finde und zeige den GGT an
+    printf("Der GGT von %d und %d ist %d\n", zahl1, zahl2, findeGGT(zahl1, zahl2));
+
+    return 0;
+}
