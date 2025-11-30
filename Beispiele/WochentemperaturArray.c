3201b597989eb5b97f29bffefa8a7cd10903b0b9
Michael Good <michael.good@bzbs.ch>
2025-11-30 19:46:31 +0100

    Wochentemperatur

--- Beispiele/WochentemperaturArray.c
+++ Beispiele/WochentemperaturArray.c
@@ -10,17 +10,43 @@ Kontakt:    michael.good@outlook.com
 
 int main() {
     float temperatur[7] = {2.1, 3.4, 5.0, 1.8, -0.5, 0.0, 4.2};
-
+    int n = 7;
     float summe = 0;
 
-    for (int i = 0; i < 7; i++) {
+    // Mittelwert berechnen
+    for (int i = 0; i < n; i++) {
         summe += temperatur[i];
     }
+    float durchschnitt = summe / n;
+    printf("Durchschnitt: %.2f %cC\n", durchschnitt, 248);
 
-    float durchschnitt = summe / 7;
+    // --- Median berechnen ---
+    // Kopie des Arrays erstellen, damit Originalwerte erhalten bleiben
+    float temp_sorted[7];
+    for (int i = 0; i < n; i++) {
+        temp_sorted[i] = temperatur[i];
+    }
 
-    printf("\nDurchschnitt: %.2f %cC\n", durchschnitt, 248);
+    // Einfaches Bubble Sort zum Sortieren
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (temp_sorted[j] > temp_sorted[j + 1]) {
+                float tmp = temp_sorted[j];
+                temp_sorted[j] = temp_sorted[j + 1];
+                temp_sorted[j + 1] = tmp;
+            }
+        }
+    }
 
+    float median;
+    if (n % 2 == 1) {  // ungerade Anzahl
+        median = temp_sorted[n / 2];
+    } else {           // gerade Anzahl
+        median = (temp_sorted[n/2 - 1] + temp_sorted[n/2]) / 2;
+    }
+
+    printf("Median: %.2f %cC\n", median, 248);
 
     return 0;
 }
+
