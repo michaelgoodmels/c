3676e72b550cb4eaa0d69d63ac5fa351e0c11f7d
Michael Good <michael.good@bzbs.ch>
2025-11-30 19:36:57 +0100

    Wochentemperatur

---
+++ Beispiele/WochentemperaturArray.c
@@ -0,0 +1,25 @@
+/*
+Projekt:    Wochentemperatur Array
+Autor:      Michael Good
+Datum:      29.11.2025
+Version:    0.1
+Kontakt:    michael.good@outlook.com
+*/
+
+#include <stdio.h>
+
+int main() {
+    float temperatur[7] = {2.1, 3.4, 5.0, 1.8, -0.5, 0.0, 4.2};
+
+    float summe = 0;
+
+    for (int i = 0; i < 7; i++) {
+        summe += temperatur[i];
+    }
+
+    float durchschnitt = summe / 7;
+
+    printf("Durchschnittstemperatur der Woche: %.2f°C\n", durchschnitt);
+
+    return 0;
+}
